7b563e0d85a2d88cd32cb532eb7d2bcd4535abad
Rafal Zado <144228957+Zadowolony@users.noreply.github.com>
2023-12-11 14:51:49 +0100

    homepage aanpassingen

--- JavaScript/food_menu.js
+++ JavaScript/food_menu.js
@@ -1,12 +1,12 @@
-var content1 = document.getElementById("content1");
-var content2 = document.getElementById("content2");
-var content3 = document.getElementById("content3");
-var content4 = document.getElementById("content4");
+let content1 = document.getElementById("content1");
+let content2 = document.getElementById("content2");
+let content3 = document.getElementById("content3");
+let content4 = document.getElementById("content4");
 
-var btn1 = document.getElementById("btn1");
-var btn2 = document.getElementById("btn2");
-var btn3 = document.getElementById("btn3");
-var btn4 = document.getElementById("btn4");
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
 
 function openCourse() {
     content1.style.transform = "translateX(0)";
