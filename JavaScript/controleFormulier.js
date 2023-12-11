861b55f08e67f68c1886459df4c85005aad2bdb5
Rafal Zado <144228957+Zadowolony@users.noreply.github.com>
2023-12-11 08:57:05 +0100

    formulier

---
+++ JavaScript/controleFormulier.js
@@ -0,0 +1,271 @@
+"use strict"
+
+// definitie globale variabelen: helemaal bovenaan
+
+
+
+let firstNameTxt;
+let lastNameTxt;
+let emailTxt;
+let telTxt;
+
+let checkTxt;
+
+let selectedLunchDinerTxt;
+let lunchDinerTxt;
+
+let selectedPersonsTxt;
+let personsTxt;
+
+let timeTxt;
+let dateTxt;
+
+let tableTxt;
+
+
+let allesCorrectIngevuld = true;
+
+
+// definitie helper functies: boven hoofdfunctie
+
+// function controleerVoorwaardenLunchDiner(){
+
+//     if(lunchDinerTxt !== "Lunch" && lunchDinerTxt !== "Diner") {
+//         document.getElementById("lunch_diner_error").innerHTML = "Choose";
+//         allesCorrectIngevuld = false;
+//     }else {
+//         document.getElementById("lunch_dinner_error").innerHTML = " ";
+//     }
+
+// }
+
+// function  controleerVoorwaardenPersons(){
+
+//     if( personsTxt !== "1" && personsTxt !== "2" && personsTxt !== "3" && personsTxt !== "4" && personsTxt !== "5" && personsTxt !== "6" ) {
+//         document.getElementById("persons_error").innerHTML = "Choose how many persons."
+//         allesCorrectIngevuld = false;
+//     }else {
+//         document.getElementById("persons_error").innerHTML = "";
+//     }
+// }
+
+function controleerVoorwaardenTime(){
+
+    if( timeTxt == ""){
+        document.getElementById("time_error").innerHTML = "Invalid time";
+        allesCorrectIngevuld = false;
+    }else {
+        document.getElementById("time_error").innerHTML = "";
+    }
+}
+
+function  controleerVoorwaardenDate(){
+
+    if(dateTxt == ""){
+        document.getElementById("date_error").innerHTML =" invalid date";
+        allesCorrectIngevuld = false;
+    }else {
+        document.getElementById("date_error").innerHTML= "";
+    }
+    
+}
+
+function controleerVoorwaardenTable() {
+    let tableRadios = document.getElementsByName("table");
+    let tableError = document.getElementById("table_error");
+    let checked = false;
+
+    for (let i = 0; i < tableRadios.length; i++) {
+        if (tableRadios[i].checked) {
+            checked = true;
+            break;
+        }
+    }
+
+    if (!checked) {
+        tableError.innerHTML = "Kies een tafeloptie.";
+        return false; // Geen tafeloptie geselecteerd
+    } else {
+        tableError.innerHTML = "";
+        return true; // Tafeloptie geselecteerd
+    }
+}
+
+
+function controleerVoorwaardenVoornaam(){
+
+   
+
+    if(firstNameTxt.length < 2 || lastNameTxt.lenght < 2) {
+        document.getElementById("firstname_error").innerHTML="It has to be 2 or more charachters";
+        document.getElementById("lastname_error").innerHTML="It has to be 2 or more charakters.";
+       
+
+        allesCorrectIngevuld = false;
+    }else {
+        document.getElementById("firstname_error").innerHTML="";
+        document.getElementById("lastname_error").innerHTML="";
+        
+    }
+
+   
+    
+}
+
+function controleerVoorwaardenEmail(){
+
+    let regExp = /^[A-Za-z][\.A-Za-z0-9+_-]+@[\.A-Za-z0-9-]+\.[A-Za-z]{2,20}$/;
+    
+   
+
+    if( regExp.test(emailTxt) == false){
+        document.getElementById("email_error").innerHTML="This email is invalid";
+        
+        allesCorrectIngevuld = false;
+
+    }else {
+        document.getElementById("email_error").innerHTML="";
+      
+      
+
+    }
+
+
+}
+
+function controleerVoorwaardenTel(){
+    let regExpTel = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/;
+
+    if( regExpTel.test(telTxt) == false){
+        document.getElementById("tel_error").innerHTML="This number is invalid";
+        allesCorrectIngevuld = false;
+
+    }else{
+        document.getElementById("tel_error").innerHTML="";
+
+    }
+
+
+}
+
+
+
+
+
+
+//definitie hoofdfunctie als laatste: verstuur()
+
+
+function verstuur() {
+
+    firstNameTxt=document.getElementById("firstname").value;
+    lastNameTxt=document.getElementById("lastname").value;
+    emailTxt=document.getElementById("email").value;
+    telTxt=document.getElementById("tel").value;
+    checkTxt=document.getElementById("checkbox").checked;
+    
+    selectedLunchDinerTxt = document.getElementById("lunch-diner").selectedIndex;
+    lunchDinerTxt = document.getElementById("lunch-diner").value;
+
+    selectedPersonsTxt = document.getElementById("persons").selectedIndex;
+    personsTxt = document.getElementById("persons").value;
+
+
+    timeTxt = document.getElementById("time").value;
+    dateTxt = document.getElementById("date").value;
+
+    let isTableValid = controleerVoorwaardenTable();
+
+   
+
+
+    allesCorrectIngevuld = true;
+
+    if(selectedLunchDinerTxt == 0){
+        document.getElementById("lunch_diner_error").innerHTML="Choose";
+        allesCorrectIngevuld= false;
+
+    }else {
+        document.getElementById("lunch-diner").innerHTML="";
+  
+        
+    }
+
+    if(selectedPersonsTxt == 0){
+        document.getElementById("persons_error").innerHTML="How many persons?";
+        allesCorrectIngevuld = false;
+    }else {
+        // personsTxt = document.getElementById("persons").value;
+        // controleerVoorwaardenPersons();
+
+        document.getElementById("persons").innerHTML="";
+    }
+
+    if(timeTxt == 0){
+        document.getElementById("time_error").innerHTML="Choose your time";
+        allesCorrectIngevuld = false;
+    }else {
+        controleerVoorwaardenTime();
+    }
+
+    if(dateTxt == 0){
+        document.getElementById("date_error").innerHTML="Choose your date";
+        allesCorrectIngevuld = false;
+    }else {
+        controleerVoorwaardenDate();
+    }
+
+    if(!isTableValid){
+        document.getElementById("table_error").innerHTML = "Kies een tafeloptie.";
+        allesCorrectIngevuld = false;
+    } else {
+        
+        controleerVoorwaardenTable();
+    }
+
+
+
+    if(firstNameTxt.length==0 || lastNameTxt.lenght==0){
+        document.getElementById("firstname_error").innerHTML="Your Name";
+        document.getElementById("lastname_error").innerHTML="Your last Name";
+       
+        allesCorrectIngevuld = false;
+
+    } else  {
+       controleerVoorwaardenVoornaam();
+              
+    }
+
+    if(emailTxt==0){
+        document.getElementById("email_error").innerHTML="Your Email";
+        allesCorrectIngevuld = false;
+
+    }else {
+
+        controleerVoorwaardenEmail();
+
+    }
+
+    if(telTxt==0){
+        document.getElementById("tel_error").innerHTML="Your number";
+        allesCorrectIngevuld = false;
+    }else {
+
+        controleerVoorwaardenTel();
+    }
+
+    if(checkTxt == 0){
+        document.getElementById("checkbox_error").innerHTML="Pls accept the terms and conditions."
+        allesCorrectIngevuld = false;
+    }
+
+
+
+    if(allesCorrectIngevuld){
+        document.write("Naam is correct ingevuld");
+    }
+
+
+    
+
+}
